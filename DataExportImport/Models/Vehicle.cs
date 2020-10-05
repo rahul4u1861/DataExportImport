97d5c8e2e54f348eb4417925c0b62286d5a09cd9
Rahul Dwivedi <rahul.dwivedi811@gmail.com>
2020-10-05 12:12:19 +0530

    Added models and views for DataExport
    Added models and views for DataExport

---
+++ DataExportImport/Models/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace DataExportImport.Models
+{
+    public class Vehicle
+    {
+        public int ID { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Odometer { get; set; }
+    }
+}
